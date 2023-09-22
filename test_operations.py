1c33600f6959698804d0aa8ce45dfce77918f653
Mehmet Yıldız <yildiz.mehmet@metu.edu.tr>
2023-09-22 10:34:49 +0300

    decimal fixed

--- test_operations.py
+++ test_operations.py
@@ -8,8 +8,8 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
     assert add(0, 0) == 0
-    assert add(0.1, 0.2) == 0.3
-    assert add(-0.1, -0.1) == -0.2
+    assert round(add(0.1, 0.2), 2) == 0.3
+    assert round(add(-0.1, -0.1), 2) == -0.2
 
 def test_add_missing_value():
     with pytest.raises(ValueError) as excinfo:
@@ -26,9 +26,9 @@ def test_subtract():
     assert subtract(-1, 1) == -2
     assert subtract(-1, -1) == 0
     assert subtract(0, 0) == 0
-    assert subtract(0.1, 0.2) == -0.1
-    assert subtract(-0.1, -0.1) == 0
-    assert subtract(0.2, 0.1) == 0.1
+    assert round(subtract(0.1, 0.2), 2) == -0.1
+    assert round(subtract(-0.1, -0.1), 2) == 0
+    assert round(subtract(0.2, 0.1), 2) == 0.1
 
 def test_subtract_missing_value():
     with pytest.raises(ValueError) as excinfo:
@@ -44,10 +44,10 @@ def test_multiply():
     assert multiply(-1, 1) == -1
     assert multiply(-1, -1) == 1
     assert multiply(0, 0) == 0
-    assert multiply(0.1, 0.2) == 0.02
-    assert multiply(-0.1, -0.1) == 0.01
-    assert multiply(0.1, -0.1) == -0.01
-    assert multiply(-0.1, 0.1) == -0.01
+    assert round(multiply(0.1, 0.2), 2) == 0.02
+    assert round(multiply(-0.1, -0.1), 2) == 0.01
+    assert round(multiply(0.1, -0.1), 2) == -0.01
+    assert round(multiply(-0.1, 0.1), 2) == -0.01
     assert multiply(0, 5) == 0
 
 def test_multiply_missing_value():
@@ -64,10 +64,10 @@ def test_divide():
     assert divide(-1, 1) == -1
     assert divide(10, 2) == 5
     assert divide(0, 1) == 0
-    assert divide(0.1, 0.2) == 0.5
-    assert divide(-0.1, -0.1) == 1
-    assert divide(0.1, -0.1) == -1
-    assert divide(-0.1, 0.1) == -1
+    assert round(divide(0.1, 0.2), 2) == 0.5
+    assert round(divide(-0.1, -0.1), 2) == 1
+    assert round(divide(0.1, -0.1), 2) == -1
+    assert round(divide(-0.1, 0.1), 2) == -1
 
 def test_divide_by_zero():
     with pytest.raises(ValueError) as excinfo:
