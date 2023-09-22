edd1d09787b6f2184dbc57d6d6cf07fe6eb7c460
Mehmet Yıldız <yildiz.mehmet@metu.edu.tr>
2023-09-22 11:10:55 +0300

    1 test should not pass

--- test_operations.py
+++ test_operations.py
@@ -11,6 +11,7 @@ def test_add():
     assert round(add(0.1, 0.2), 2) == 0.3
     assert round(add(-0.1, -0.1), 2) == -0.2
 
+
 def test_add_missing_value():
     with pytest.raises(ValueError) as excinfo:
         add(2, None)
@@ -18,6 +19,7 @@ def test_add_missing_value():
     with pytest.raises(ValueError) as excinfo:
         add(None, 3)
     assert str(excinfo.value) == "Missing value: a=None, b=3"
+
 
 # Define test cases for the 'subtract' function
 def test_subtract():
@@ -30,6 +32,7 @@ def test_subtract():
     assert round(subtract(-0.1, -0.1), 2) == 0
     assert round(subtract(0.2, 0.1), 2) == 0.1
 
+
 def test_subtract_missing_value():
     with pytest.raises(ValueError) as excinfo:
         subtract(None, 3)
@@ -37,6 +40,7 @@ def test_subtract_missing_value():
     with pytest.raises(ValueError) as excinfo:
         subtract(2, None)
     assert str(excinfo.value) == "Missing value: a=2, b=None"
+
 
 # Define test cases for the 'multiply' function
 def test_multiply():
@@ -50,6 +54,7 @@ def test_multiply():
     assert round(multiply(-0.1, 0.1), 2) == -0.01
     assert multiply(0, 5) == 0
 
+
 def test_multiply_missing_value():
     with pytest.raises(ValueError) as excinfo:
         multiply(2, None)
@@ -57,6 +62,7 @@ def test_multiply_missing_value():
     with pytest.raises(ValueError) as excinfo:
         multiply(None, 3)
     assert str(excinfo.value) == "Missing value: a=None, b=3"
+
 
 # Define test cases for the 'divide' function
 def test_divide():
@@ -67,12 +73,15 @@ def test_divide():
     assert round(divide(0.1, 0.2), 2) == 0.5
     assert round(divide(-0.1, -0.1), 2) == 1
     assert round(divide(0.1, -0.1), 2) == -1
-    assert round(divide(-0.1, 0.1), 2) == -1
+    # assert round(divide(-0.1, 0.1), 2) == -1
+    assert divide(-0.1, 0.1) == -1
+
 
 def test_divide_by_zero():
     with pytest.raises(ValueError) as excinfo:
         divide(2, 0)
     assert str(excinfo.value) == "Cannot divide by zero"
+
 
 def test_divide_missing_value():
     with pytest.raises(ValueError) as excinfo:
@@ -81,6 +90,7 @@ def test_divide_missing_value():
     with pytest.raises(ValueError) as excinfo:
         divide(2, None)
     assert str(excinfo.value) == "Missing value: a=2, b=None"
+
 
 # Run the tests
 if __name__ == '__main__':
